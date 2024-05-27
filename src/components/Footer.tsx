ca6c319c231bd3ce6646e0edeb08e41deb76625d
Giorgi Metreveli <g.metreveli1993@gmail.com>
2024-05-27 12:57:17 +0400

    footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,134 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div className="">Footer</div>;
+  return (
+    <div className="py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-gray-100 text-sm mt-24">
+      {/* Top */}
+      <div className=" flex flex-col md:flex-row justify-between gap-24">
+        {/* Left */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <Link href={"/"}>
+            <div className="text-2xl tracking-wider">GIO</div>
+          </Link>
+          <p>
+            3727 Lightning Way, Central PLaza, Willowbrook, CA 90210, United
+            States
+          </p>
+          <span className="font-semibold">Holla@gio.dev</span>
+          <span className="font-semibold">+1 234 567 890</span>
+          <div className="flex gap-6">
+            <Image
+              src={"/facebook.png"}
+              alt=" some pic"
+              width={16}
+              height={16}
+            />
+            <Image
+              src={"/instagram.png"}
+              alt=" some pic"
+              width={16}
+              height={16}
+            />
+            <Image
+              src={"/youtube.png"}
+              alt=" some pic"
+              width={16}
+              height={16}
+            />
+            <Image
+              src={"/pinterest.png"}
+              alt=" some pic"
+              width={16}
+              height={16}
+            />
+            <Image src={"/x.png"} alt=" some pic" width={16} height={16} />
+          </div>
+        </div>
+        {/* Center */}
+        <div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">Company</h1>
+            <div className="flex flex-col gap-6">
+              <Link href={""}>About Us</Link>
+              <Link href={""}>Careers</Link>
+              <Link href={""}>Affiliates</Link>
+              <Link href={""}>Blog</Link>
+              <Link href={""}>Contact Us</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">Shop</h1>
+            <div className="flex flex-col gap-6">
+              <Link href={""}>New Arrivals</Link>
+              <Link href={""}>Accessories</Link>
+              <Link href={""}>Men</Link>
+              <Link href={""}>Women</Link>
+              <Link href={""}>All Products</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">Help</h1>
+            <div className="flex flex-col gap-6">
+              <Link href={""}>Customer Service</Link>
+              <Link href={""}>My Account</Link>
+              <Link href={""}>Find a Store</Link>
+              <Link href={""}>Legal & Privacy</Link>
+              <Link href={""}>Gift Card</Link>
+            </div>
+          </div>
+        </div>
+        {/* Right */}
+        <div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+          <h1 className="font-medium">Subscribe</h1>
+          <p>
+            Be the first to get the latest news about trends, promotions, and
+            much more!
+          </p>
+          <div className="flex">
+            <input
+              type="email"
+              placeholder="Email Address"
+              className="p-4 w-3/4"
+            />
+            <button className="w-1/4 bg-gio text-white">Join</button>
+          </div>
+          <span className="font-semibold">Secure Payments</span>
+          <div className="flex justify-between">
+            <Image
+              src={"/discover.png"}
+              alt=" some pic"
+              width={40}
+              height={20}
+            />
+            <Image src={"/skrill.png"} alt=" some pic" width={40} height={20} />
+            <Image src={"/paypal.png"} alt=" some pic" width={40} height={20} />
+            <Image
+              src={"/mastercard.png"}
+              alt=" some pic"
+              width={40}
+              height={20}
+            />
+            <Image src={"/visa.png"} alt=" some pic" width={40} height={20} />
+          </div>
+        </div>
+      </div>
+      {/* Bottom */}
+      <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
+        <div className="">&copy; 2024 Gio Shop</div>
+        <div className="flex flex-col gap-8 md:flex-row">
+          <div className="">
+            <span className="text-gray-500 mr-4">Language</span>
+            <span className="font-medium">United States | English</span>
+          </div>
+          <div className="">
+            <span className="text-gray-500 mr-4">Currency</span>
+            <span className="font-medium">$ USD</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
